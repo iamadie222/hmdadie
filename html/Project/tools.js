0f6a8aca19f5d4eb65d9def167767061a31cab14
hamid lohar <hmdlohar@gmail.com>
2018-03-25 21:25:47 +0530

    effect hmd

--- html/Project/tools.js
+++ html/Project/tools.js
@@ -85,6 +85,62 @@ function circleMouseMove(e){
 function setScaleMargin(per){
     $("#workCircle").css("margin-left",(per *4)-10);
 }
+
+
+function saveImage(){
+    canvas =document.createElement("canvas");
+    canvas.height=$("#workImage").height();
+    canvas.width=$("#workImage").width();
+    var ctx=canvas.getContext("2d");
+    ctx.drawImage($("#workImage")[0],0,0);
+    ctx.filter = getComputedStyle($("#workImage")[0]).filter;
+    const url = canvas.toDataURL(`image/png`);
+    download(url,"lohar");
+    
+    $("#workArea").append(canvas)
+}
+function download(url, fullName) {
+    const anchor = document.createElement('a')
+    anchor.href = url
+    anchor.setAttribute('download', fullName)
+  
+    anchor.click();
+  }
+  function download2(can){
+  var link = document.createElement('a');
+  link.href = can.toDataURL();
+  link.download =  "asdfsdf.png";
+  
+  // create a mouse event
+  var event = new MouseEvent('click');
+  
+  // dispatching it will open a save as dialog in FF
+  link.dispatchEvent(event);
+  }
+  function screenshot(imgNode, format = 'png', quality = 0.97) {
+    const canvas = document.createElement('canvas')
+    window.canvas=canvas;
+    canvas.width = imgNode.width
+    canvas.height = imgNode.height
+  
+    const context = canvas.getContext('2d')
+    context.filter = getComputedStyle(imgNode).filter
+  
+    imgNode.setAttribute('crossOrigin', 'anonymous')
+  
+    context.drawImage(imgNode, 0, 0, canvas.width, canvas.height)
+    const url = canvas.toDataURL(`image/${format}`, quality)
+  
+    return {
+      url,
+      then: (cb) => {
+        cb(url)
+      },
+      download: (name = 'image') => {
+        download2(canvas, `${name}.${format}`)
+      }
+    }
+  }
 $("#imageInput").change(function(){
     var r=new FileReader();
     r.readAsDataURL(this.files[0]);
@@ -93,9 +149,14 @@ $("#imageInput").change(function(){
         var img=new Image();
         img.src=res.target.result;
         img.id="workImage";
-        $("#workArea").append(img);
-        
-        
-        
+        $("#workArea").append(img); 
     }
+});
+$("#saveImage").click(function(){
+    if($("#workImage").length==0){
+        alert("select Image first");
+        return;
+    }
+    screenshot($("#workImage")[0]).download();
+    
 });
